1ef70140c529f5bfcc8d3556a780e140a9ac2de9
Loqh <guillaume.pinheiro@edu.itescia.fr>
2021-03-13 12:47:47 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BO3_Custom_Maps_Installer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
